64cb1aee0e5fdfdd433db60f59eb5e6cc9a6e4b5
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:11:19 +0100

    Add queen movement

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,20 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        let moves = [];
+
+        for (let i = 0; i < 8; i++) {
+            moves.push(Square.at(currentSquare.row, i));
+            moves.push(Square.at(i, currentSquare.col));
+            moves.push(Square.at(currentSquare.row + i, currentSquare.col + i));
+            moves.push(Square.at(currentSquare.row + i, currentSquare.col - i));
+            moves.push(Square.at(currentSquare.row - i, currentSquare.col + i));
+            moves.push(Square.at(currentSquare.row - i, currentSquare.col - i));
+        }
+
+        const inBounds = (square) => square.row >= 0 && square.row < 8 && square.col >= 0 && square.col < 8;
+
+        return moves.filter((square) => !square.equals(currentSquare) && inBounds(square));
     }
 }
