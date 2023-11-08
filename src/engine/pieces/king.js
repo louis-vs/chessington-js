c8b138f8876c4c96b056e298c8bc26cba7310c03
Louis Van Steene <Louis.VanSteene@softwire.com>
2023-11-08 17:01:44 +0000

    Add king movement

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,15 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        let moves = [];
+
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                moves.push(Square.at(currentSquare.row + i, currentSquare.col + j));
+            }
+        }
+
+        return moves.filter((square) => !square.equals(currentSquare));
     }
 }
