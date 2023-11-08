0e63012de37d8518a943970b061acb4346f6cf4c
Louis Van Steene <Louis.VanSteene@softwire.com>
2023-11-08 16:35:43 +0000

    Add bishop movement

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,16 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        let moves = [];
+
+        for (let i = -7; i < 8; i++) {
+            moves.push(Square.at(currentSquare.row + i, currentSquare.col + i));
+            moves.push(Square.at(currentSquare.row + i, currentSquare.col - i));
+        }
+
+        const inBounds = (square) => square.row >= 0 && square.row < 8 && square.col >= 0 && square.col < 8;
+
+        return moves.filter((square) => !square.equals(currentSquare) && inBounds(square));
     }
 }
