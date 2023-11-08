7c66d5189c473e97280334085a60518a11558560
Louis Van Steene <Louis.VanSteene@softwire.com>
2023-11-08 16:16:18 +0000

    Add rook movement

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +7,14 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        let moves = [];
+
+        for (let i = 0; i < 8; i++) {
+            moves.push(Square.at(currentSquare.row, i));
+            moves.push(Square.at(i, currentSquare.col));
+        }
+
+        return moves.filter((square) => !square.equals(currentSquare));
     }
 }
